cf9385c9e7f87ac6140cd43f668e3e104dc05695
Jesse James Lord <zylick@gmail.com>
2019-01-26 15:08:22 -0800

    Updaing the InitialManager to load into the game.
    Working on updating the initial manager to load into the game

--- master/SkullPants/Assets/scripts/managers/InitialManager.cs
+++ master/SkullPants/Assets/scripts/managers/InitialManager.cs
@@ -7,28 +7,31 @@ public class InitialManager : MonoBehaviour
 {
     //variables to represent the canvasGroups
 
-    public CanvasGroup mUnityCanvasGroup;
-    public CanvasGroup mGGJCanvasGroup;
+    public GameObject mUnityIcon;
+    public GameObject mGGJIcon;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("StartSceneOne");
-        
     }
 
     protected IEnumerator StartSceneOne()
     {
         //Pull up a wait for one seconds
         //Pop the Unity Logo
+        mUnityIcon.SetActive(true);
         yield return new WaitForSeconds(3.0f);
+        mUnityIcon.SetActive(false);
 
         //Pop the Global Game Jam Logo
+        mGGJIcon.SetActive(true);
         yield return new WaitForSeconds(3.0f);
+        mGGJIcon.SetActive(false);
 
         //Load game?
 
         //Load the next scene
-        //yield return SceneManager.LoadSceneAsync("Scene1");
+        yield return SceneManager.LoadSceneAsync("Scene1");
     }
 }
