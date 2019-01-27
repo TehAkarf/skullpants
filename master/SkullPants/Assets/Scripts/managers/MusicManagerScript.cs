caeaa5b192d7b078dd8338f075eab57ac7dc2f43
GMan9506 <35857766+GMan9506@users.noreply.github.com>
2019-01-26 22:47:05 -0800

    Commit Merge

--- master/SkullPants/Assets/Scripts/managers/MusicManagerScript.cs
+++ master/SkullPants/Assets/Scripts/managers/MusicManagerScript.cs
@@ -52,7 +52,7 @@ public class MusicManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Plus) || Input.GetKeyUp(KeyCode.Equals)) // Raise volume in 0.1 increments
+        if (!Input.GetKeyDown(KeyCode.LeftShift) && (Input.GetKey(KeyCode.Plus) || Input.GetKey(KeyCode.Equals))) // Raise volume in 0.1 increments
         {
             if (volume < 1.0f)
             {
@@ -64,7 +64,7 @@ public class MusicManagerScript : MonoBehaviour
             }
             ChangeVolume(volume);
         }
-        if (Input.GetKeyUp(KeyCode.Minus)) // Lower volume in 0.1 increments
+        if (!Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.Minus)) // Lower volume in 0.1 increments
         {
             if (volume > 0.0f)
             {
